268b0b288b3532f28e9b907532dfd1cd99539d5f
ayush <ayushjhasahab07@gmail.com>
2025-06-04 10:02:09 +0530

    some updates

--- react+cdn_day1/app.js
+++ react+cdn_day1/app.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+
 const parent = React.createElement(
   "div",
   { id: "parent" },
@@ -5,9 +9,7 @@ const parent = React.createElement(
     "div",
     { id: "child" },
     [React.createElement("h1", { id: "heading" }, "kaise ho mitar"),
-    React.createElement("h2 ", { id: "heading" }, "kaise ho mitar2")],
-
-
+    React.createElement("h2", { id: "heading" }, "kaisehsdfsdg ho mitar2")],
   )
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
